b2056883dba389abc40b9b4fede114c2654eec60
Milzan Malik <38238691+Skyleaft@users.noreply.github.com>
2020-07-30 21:23:36 +0700

    initial build

---
+++ KOST APP/Window_Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KOST_APP
+{
+    /// <summary>
+    /// Interaction logic for Window_Login.xaml
+    /// </summary>
+    public partial class Window_Login : Window
+    {
+        public Window_Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
